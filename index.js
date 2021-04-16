c60b2bdc4a9bede91de016edcc5f1cca5d5d80a2
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-04-16 13:01:47 +0200

    first draft

---
+++ index.js
@@ -0,0 +1,38 @@
+function isRelative(path) {
+  return path.startsWith("../");
+}
+
+function isSameFolder(path) {
+  return path.startsWith("./");
+}
+
+const message = "import statements should have an absolute path";
+
+module.exports = {
+  rules: {
+    "no-relative-imports": {
+      create: function (context) {
+        const { allowSameFolder } = context.options[0] || {};
+
+        return {
+          ImportDeclaration: function (node) {
+            const path = node.source.value;
+            if (isRelative(path)) {
+              context.report({
+                node,
+                message: message,
+              });
+            }
+
+            if (isSameFolder(path) && !allowSameFolder) {
+              context.report({
+                node,
+                message: message,
+              });
+            }
+          },
+        };
+      },
+    },
+  },
+};
