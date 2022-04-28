f5d68c65ea2d5cee781d408288ebf68674711701
Maarten Veenstra <maartenveenstra@gmail.com>
2022-04-28 17:11:26 +0200

    fix parentfolder check

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ function isParentFolder(relativeFilePath, context, rootDir) {
   const absoluteRootPath = context.getCwd() + (rootDir !== '' ? path.sep + rootDir : '');
   const absoluteFilePath = path.join(path.dirname(context.getFilename()), relativeFilePath)
 
-  return relativeFilePath.startsWith("../") && (rootDir === '' || absoluteFilePath.startsWith(absoluteRootPath));
+  return relativeFilePath.startsWith("../") && (
+    rootDir === '' ||
+    (absoluteFilePath.startsWith(absoluteRootPath) &&
+    context.getFilename().startsWith(absoluteRootPath))
+  );
 }
 
 function isSameFolder(path) {
